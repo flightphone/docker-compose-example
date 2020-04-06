8e42e2c6fd01174b4b580653a61d6d15260d907b
Efim Manevich <efimmanevich@gmail.com>
2020-04-06 09:28:46 +0300

    потоки через сохранение в файлы

--- unoform/app.js
+++ unoform/app.js
@@ -5,32 +5,37 @@ var upload = multer();
 const fetch = require('node-fetch');
 var fs = require('fs');
 var FormData = require('form-data');
-var Duplex = require('stream').Duplex; 
+var Duplex = require('stream').Duplex;
 
 var app = express();
 
-var uploadfile = function (req, res)
-{
+var uploadfile = function (req, res) {
   var filename = req.file.originalname.split(".")[0] + ".pdf";
   var url = 'http://84.201.148.77:3000/unoconv/pdf';
-  var form = new FormData();
-  var stream = new Duplex();
-  stream.push(req.file.buffer);
-  stream.push(null);
-  //var readStream = new stream.PassThrough();
-  //readStream.end(req.file.buffer);
-  form.append('file', fs.createReadStream("aaa.docx"));
-  fetch(url, { 
-    method: 'POST', 
-    body: form
+  var tmpfile = './web/f' + Math.random().toString();
+  var tmpfilepdf = tmpfile + '.pdf';
+  fs.writeFile(tmpfile, req.file.buffer, function (err) {
+    var form = new FormData();
+    form.append('file', fs.createReadStream(tmpfile));
+    fetch(url, {
+      method: 'POST',
+      body: form
     })
-    .then(r => r.buffer())
-    .then(buf => {
-      fs.writeFile("aaa.pdf", buf, function (err) {
-        return;
-    });
-    res.send("ok");
-    });
+      .then(r => r.buffer())
+      .then(buf => {
+        fs.writeFile(tmpfilepdf, buf, function (err) {
+          res.download(tmpfilepdf, filename, function (err) {
+            fs.unlink(tmpfilepdf, function(err){});
+            fs.unlink(tmpfile, function(err){});
+          });
+        });
+
+      });
+
+
+  });
+
+
 }
 
 app.use(bodyParser.json()); // support json encoded bodies
