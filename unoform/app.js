9bd0a5a809bbe6eb0e2743d117b0cd4b4ed166b5
Efim Manevich <efimmanevich@gmail.com>
2020-04-07 08:48:24 +0300

    промежуточный результат

--- unoform/app.js
+++ unoform/app.js
@@ -5,7 +5,8 @@ var upload = multer();
 const fetch = require('node-fetch');
 var fs = require('fs');
 var FormData = require('form-data');
-var Duplex = require('stream').Duplex;
+//var streamifier = require('streamifier');
+
 
 var app = express();
 var convhost = process.env.CONV_HOST || '84.201.148.77'
@@ -14,34 +15,28 @@ var uploadfile = function (req, res) {
   var filename = req.file.originalname.split(".")[0] + ".pdf";
   var url = 'http://' + convhost + ':' + convport + '/unoconv/pdf';
   var tmpfile = './web/f' + Math.random().toString();
-  var tmpfilepdf = tmpfile + '.pdf';
-  fs.writeFile(tmpfile, req.file.buffer, function (err) {
+  fs.writeFile(tmpfile, req.file.buffer, function (err) 
+  {
     var form = new FormData();
     form.append('file', fs.createReadStream(tmpfile));
+    //form.append('file', streamifier.createReadStream(req.file.buffer));
+
     fetch(url, {
       method: 'POST',
       body: form
-    })
-      .then(r => r.buffer())
+    }).then(r => r.buffer())
       .then(buf => {
-        /*
-        fs.writeFile(tmpfilepdf, buf, function (err) {
-          res.download(tmpfilepdf, filename, function (err) {
-            fs.unlink(tmpfilepdf, function(err){});
-            fs.unlink(tmpfile, function(err){});
-          });
-        */
 
         res.writeHead(200, {
           'Content-Type': 'application/pdf',
-          'Content-Disposition': 'attachment; filename=' + filename,
+          'Content-Disposition': 'attachment; filename="' + encodeURIComponent(filename) + '"',
           'Content-Length': buf.length
         });
         res.end(buf);
+        fs.unlink(tmpfile, function (err){});
       });
 
   });
-
 }
 
 app.use(bodyParser.json()); // support json encoded bodies
@@ -52,3 +47,38 @@ app.post('/upload', upload.single('file'), uploadfile);
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
 });
+
+
+function convert(buf, chunkSize) {
+  if (typeof buf === 'string') {
+    buf = Buffer.from(buf, 'utf8')
+  }
+  if (!Buffer.isBuffer(buf)) {
+    throw new TypeError(`"buf" argument must be a string or an instance of Buffer`)
+  }
+
+  const reader = new Readable()
+  const hwm = reader._readableState.highWaterMark
+
+  // If chunkSize is invalid, set to highWaterMark.
+  if (!chunkSize || typeof chunkSize !== 'number' || chunkSize < 1 || chunkSize > hwm) {
+    chunkSize = hwm
+  }
+
+  const len = buf.length
+  let start = 0
+
+  // Overwrite _read method to push data from buffer.
+  reader._read = function () {
+    while (reader.push(
+      buf.slice(start, (start += chunkSize))
+    )) {
+      // If all data pushed, just break the loop.
+      if (start >= len) {
+        reader.push(null)
+        break
+      }
+    }
+  }
+  return reader
+}
